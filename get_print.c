12882f7614d26ff787956d39114da5eb1919d0c8
mazinmohd <mazenomermazen2002@gmail.com>
2023-09-18 19:58:26 +0300

    a

--- get_print.c
+++ get_print.c
@@ -1,15 +1,9 @@
 #include "main.h"
 
 /**
- * get_print - selects the right printing function
- * depending on the conversion specifier passed to _printf
- * @s: character that holds the conversion specifier
- * Description: the function loops through the structs array
- * func_arr[] to find a match between the specifier passed to _printf
- * and the first element of the struct, and then the approriate
- * printing function
- * Return: a pointer to the matching printing function
- * Authors: Ehoneah Obed & Abdulhakeem Badejo
+ * get_print - select the printer
+ * @s: char 
+ * Return: pointer
  */
 int (*get_print(char s))(va_list, flags_t *)
 {
